b5dae4b224e63fcd5bfcf695042e9abcf87a33f6
wulangyun <wulangyun@xiaohongshu.com>
2022-08-20 17:27:10 +0800

    冲突解决

--- Demo/src/TestIgnore.java
+++ Demo/src/TestIgnore.java
@@ -1,6 +1,5 @@
 public class TestIgnore {
-  public static void main(String[] args){
-    System.out.println("远程修改");
-    System.out.print("远程修改不换行");
-  }
+    public static void main(String[] args) {
+        System.out.println("本地修改");
+    }
 }
