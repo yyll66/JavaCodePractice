c3c1dcfb569d75c75108f99367f644abc240a5e2
yyll66 <67272700+yyll66@users.noreply.github.com>
2022-08-20 17:46:28 +0800

    Update TestIgnore.java

--- Demo/src/TestIgnore.java
+++ Demo/src/TestIgnore.java
@@ -1,6 +1,7 @@
 public class TestIgnore {
     public static void main(String[] args) {
         System.out.println("本地修改");
-        System.out.println("远程修改");
+        System.out.println("远程修改1");
+        System.out.println("远程修改2");
     }
 }
